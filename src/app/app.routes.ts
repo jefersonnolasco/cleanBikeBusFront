44e6032b48fbdedc01d62a1b95df44e1037988c9
Jeferson Nolasco <jeferson.nolasco@labatt.com>
2024-09-09 16:05:59 +0100

    first

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,3 @@
 import { Routes } from '@angular/router';
-import { PrimeiroComponent } from './pages/primeiro/primeiro.component';
-import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },{
-    path: 'primeiro',
-    component: PrimeiroComponent
-  },
-  {
-    path: 'portfolio',
-    component: HomeComponent
-  }
-];
+export const routes: Routes = [];
